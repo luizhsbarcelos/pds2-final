888c41d1ec24373d35404b00787d839bce4e918f
Aninha <134537320+analuisamessias@users.noreply.github.com>
2023-11-28 13:24:32 -0300

    Create fita.cpp

---
+++ src/implementacao/fita.cpp
@@ -0,0 +1,21 @@
+#include "fita.hpp"
+#include <iostream>
+
+Fita::Fita(int cod, std::string titulo, int qtd, bool fita_rebobinada)
+    : Cadastro_filme(cod, titulo, qtd), fita_rebobinada(fita_rebobinada) {}
+
+bool Fita::rebobina() const {
+    return fita_rebobinada;
+}
+
+double Fita::valor_locacao(int dias) {
+    return 5 * dias + (fita_rebobinada ? 0 : 2);
+}
+
+std::string Fita::tipo_locacao() const {
+    return "FITA";
+}
+
+void Fita::imprime_relatorio() {
+    std::cout << "FITA - Codigo: " << get_codigo() << ", Titulo: " << get_titulo() << ", Rebobinada: " << (fita_rebobinada ? "Sim" : "Nao") << "\n";
+}
