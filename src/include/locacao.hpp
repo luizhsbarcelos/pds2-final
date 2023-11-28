333830035bc365c86b772fb16c032d9d64e3cb47
Bernardo Rossetti <134241405+Brotti@users.noreply.github.com>
2023-11-27 22:46:45 -0300

    Adicao da filetree

---
+++ src/include/locacao.hpp
@@ -0,0 +1,20 @@
+#ifndef
+#define LOCACAO_HPP
+
+#include <vector>
+#include <ctime>
+#include <string>
+
+class locacao 
+{
+private:
+	int _id_locacao;
+	Cliente* _cliente;
+	std::vector<Filme>;
+	int _qtd_dias;		
+
+public:
+	void set_id(int _id);
+	void 
+}
+#endif
