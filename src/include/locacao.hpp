c6b658a2950dfa1459537bb08bd3286e1c4ddd48
Bernardo Rossetti <134241405+Brotti@users.noreply.github.com>
2023-11-28 20:29:00 -0300

    Add files via upload

--- src/include/locacao.hpp
+++ src/include/locacao.hpp
@@ -1,20 +1,30 @@
-#ifndef
+#ifndef LOCACAO_HPP
 #define LOCACAO_HPP
 
 #include <vector>
-#include <ctime>
+#include <ctime> // lib para manipulacao de datas
 #include <string>
 
-class locacao 
+class Locacao 
 {
 private:
+	// Atributos da classe 
 	int _id_locacao;
 	Cliente* _cliente;
-	std::vector<Filme>;
-	int _qtd_dias;		
+	std::vector<int> _filmes_alugados;
+	// atributo datas	
+	// Vetor que contem todas as locaoes
+	static std::vector<*Locacao> _lista_de_locacoes; 
 
 public:
-	void set_id(int _id);
-	void 
-}
+	// Construtor e destrutor
+	Locacao(int id, Cliente* cliente, std::vector<int> filmes);
+	~Locacao(Cliente* cliente, int dias_alugado);
+	Locacao::Locacao();
+	Locacao::~Locacao();
+
+
+
+};
+
 #endif
