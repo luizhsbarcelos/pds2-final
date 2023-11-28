95b623811e1cdb305fee68f45e9afc2d765f528d
Aninha <134537320+analuisamessias@users.noreply.github.com>
2023-11-28 13:20:12 -0300

    Create fita.hpp

---
+++ src/include/fita.hpp
@@ -0,0 +1,18 @@
+#ifndef FITA_HPP
+#define FITA_HPP
+
+#include "cadastro_filmes.hpp"
+
+class Fita : public Cadastro_filme {
+private:
+    bool fita_rebobinada;
+
+public:
+    Fita(int cod, std::string titulo, int qtd, bool fita_rebobinada);
+    bool rebobina() const;
+    double valor_locacao(int dias) const;
+    std::string tipo_locacao() const;
+    void imprime_relatorio() override;
+};
+
+#endif // FITA_HPP
