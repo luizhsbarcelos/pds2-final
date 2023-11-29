2aa79f3a862ebb26b6aea902e6af1e3dcb35a5a1
Bernardo Rossetti <134241405+Brotti@users.noreply.github.com>
2023-11-29 00:16:20 -0300

    Add files via upload

--- src/implementacao/locacao.cpp
+++ src/implementacao/locacao.cpp
@@ -1,20 +1,20 @@
 #include "locacao.hpp"
-#include "cliente.hpp" 
-#include "cadastro_filmes.hpp"
 
 #include <vector>
 #include <iostream>
 
 // Construtor
-Locacao::Locacao(int id, Cliente* cliente, std::vector<int> filmes_alugados) 
-    : _id_locacao(id), _cliente(cliente), _filmes(filmes_alugados)
+Locacao::Locacao(int id, std::string cpf, std::vector<int> filmes) : 
+    _id_locacao(id),
+    _cpf_cliente(cpf),
+    _filmes_alugados(filmes)
 {   
 }
 
-// Construtor e destrutor padrao
+// Construtor e destrutor padrão
 Locacao::Locacao()
 {
-	std::cout << "ERRO: Objeto instaciado indevidamente, o destrutor sera chamado" << std::endl;
+	std::cout << "ERRO: Objeto instaciado indevidamente, o destrutor será chamado" << std::endl;
 	~Locacao();
 }
 
@@ -22,32 +22,21 @@ Locacao::~Locacao()
 {
 }
 
-int Locacao::get_id_locacao()
+// Implementação dos métodos get
+int Locacao::get_id()
 {
     return _id_locacao;
 }
 
-Cliente* Locacao::get_cliente_locacao()
+std::string Locacao::get_cpf()
 {
-    return _cliente;
+    return _cpf_cliente;
 }
 
-std::vector<int> Locacao::get_filmes_alugados()
+std::vector<int> Locacao::get_filmes()
 {
     return _filmes_alugados;
 }
 
-Locacao::imprimir_locacao()
-{
-    std::cout << "==============================" << std::endl;
-    std::cout << "ID: " << get_id_locacao() << std::endl;
-    std::cout << "Cpf do cliente: " << get_id_locacao() << std::endl; 
-    std::cout << "Filmes alugados (cód.):";
-    for (int it : _filmes_alugados)
-    {
-        std::cout << " " << it << std::endl;
-    }
-    std::cout << "==============================" << std::endl << std::endl;
-}
 
 
