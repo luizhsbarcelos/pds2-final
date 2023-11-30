3d5d3b8d1c169f8f8780e8657898daa28bdfec5e
Bernardo Rossetti <134241405+Brotti@users.noreply.github.com>
2023-11-29 22:04:06 -0300

    Add files via upload

--- src/implementacao/arquivo.cpp
+++ src/implementacao/arquivo.cpp
@@ -2,12 +2,28 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+
+std::vector<std::string> Arquivo::separa_palavras(std::string str) {
+
+  std::istringstream iss(str);
+
+  std::vector<std::string> _vector_str;
+  std::string palavra;
+
+  // Extrai cada palavra da string
+  while (iss >> palavra) {
+    _vector_str.push_back(palavra);
+  }
+  return _vector_str;
+}
 
 void Arquivo::ler_arquivo() {
 
   std::cin >> _nome_arquivo;
-  std::ifstream arquivo(_path_arquivo + _nome_arquivo);
+  std::ifstream arquivo("../data/" + _nome_arquivo);
 
   // Checagem se arquivo está aberto
   if (!arquivo.is_open()) {
@@ -19,4 +35,16 @@ void Arquivo::ler_arquivo() {
     _vector_linhas.push_back(_linha);
   }
   arquivo.close();
+
+  return;
 }
+
+void Arquivo::conta_filmes() {
+  qtd_filmes_arquivo++;
+  return;
+}
+
+int Arquivo::get_qtd_filmes() { return qtd_filmes_arquivo; }
+
+Arquivo::Arquivo() {}
+Arquivo::~Arquivo() {}
