227460e8a71793c37c9a7d07e5553c08f0b7fd45
Aninha <134537320+analuisamessias@users.noreply.github.com>
2023-12-01 15:22:00 -0300

    Update fita.cpp

--- src/implementacao/fita.cpp
+++ src/implementacao/fita.cpp
@@ -1,4 +1,4 @@
-#include "../include/fita.hpp"
+#include "fita.hpp"
 #include <iostream>
 
 Fita::Fita(int qtd, int cod, std::string titulo) : Filme(qtd, cod, titulo) {}
@@ -7,16 +7,20 @@ Fita::~Fita() {
   std::cout << "Filme " << get_codigo() << " removido com sucesso\n";
 }
 
-bool Fita::rebobina() const { return fita_rebobinada; }
-
-double Fita::valor_locacao(int dias) const {
-  return 5 * dias + (fita_rebobinada ? 0 : 2);
+bool Fita::rebobina() const {
+    return fita_rebobinada;
 }
 
-std::string Fita::tipo_midia() const { return "FITA"; }
+double Fita::valor_locacao(int dias) const {
+    return 5 * dias + (fita_rebobinada ? 0 : 2);
+}
+
+std::string Fita::tipo_midia() const {
+    return "FITA";
+}
 
 void Fita::imprime_relatorio() {
-  std::cout << get_codigo() << " " << get_titulo() << " " << get_quantidade()
-            << " " << tipo_midia() << "\n";
-  return;
+    std::cout << get_codigo() << " " << get_titulo() << " " << get_quantidade()
+              << " " << tipo_midia() << "\n";
+    return;
 }
