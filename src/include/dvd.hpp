1839f83bc40e6991f723267903e1b9d4c81f64ae
Aninha <134537320+analuisamessias@users.noreply.github.com>
2023-12-01 15:20:26 -0300

    Update dvd.hpp

--- src/include/dvd.hpp
+++ src/include/dvd.hpp
@@ -1,8 +1,6 @@
 #ifndef DVD_HPP
 #define DVD_HPP
-
 #include "filme.hpp"
-
 #include <string>
 
 class DVD : public Filme {
@@ -11,10 +9,10 @@ private:
 
 public:
     DVD(int qtd, int cod, std::string titulo, std::string categoria);
-    ~DVD();
+    virtual ~DVD();
     double valor_locacao(int dias) const;
     std::string tipo_midia() const;
     void imprime_relatorio() override;
 };
 
-#endif 
+#endif
